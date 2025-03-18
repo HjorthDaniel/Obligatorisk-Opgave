68497999c75ea7a3723b817b41e250bd27763c26
Daniel Hjorth <dan003@edu.zealand.dk>
2025-03-18 14:08:46 +0100

    Add project files.

---
+++ Class.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication2
+{
+    public class Event
+    {
+
+        //Constructor
+
+        public Event(String title, int år)
+        {
+            
+        }
+       
+
+
+
+
+    }
+}
---
+++ Pages/WillieJonsson.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages
+{
+    public class WillieJonssonModel : PageModel
+    {
+        public void OnGet()
+        {
+
+
+        }
+    }
+}
